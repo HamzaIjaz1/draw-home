a9c6867c10228b389ff3714bb0d854edcbd1c793
Hamza Ijaz <34644679+HamzaIjaz1@users.noreply.github.com>
2025-12-08 22:22:54 +0500

    Updating node version files

--- helpers/start-checks.js
+++ helpers/start-checks.js
@@ -9,7 +9,9 @@ const { execSync } = require('child_process');
 const nvmrc = fs.readFileSync(path.join(__dirname, '../.nvmrc'), 'utf-8').trim();
 
 const { node } = process.versions;
-assert(node === nvmrc, `You must use Node.js v${nvmrc} for this project (${node} used)`);
+const requiredMajor = nvmrc.split('.')[0];
+const currentMajor = node.split('.')[0];
+assert(currentMajor === requiredMajor, `You must use Node.js v${requiredMajor}.x for this project (${node} used)`);
 
 const npmVersion = execSync('npm -v').toString().trim();
 assert(Number(npmVersion.split('.')[0]) === 10, `You must use NPM v10.x.x (${npmVersion} used)`);
